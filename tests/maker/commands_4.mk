9d67cf59a8749b6070c75ea3dcc19d54aa26c920
Istvan Albert <istvan.albert@gmail.com>
2025-05-14 14:44:16 -0400

    update

--- tests/maker/commands_4.mk
+++ tests/maker/commands_4.mk
@@ -1,23 +1,19 @@
-#
-# Python code may be embedded fully and gets compiled and executed
-#
-
-% NUMS = range(10)
-% VALS = [x*x for x in NUMS]
-
-<% for num, val in zip(NUMS, VALS): %>
-${num}.txt:
-	echo ${val} > ${num}.txt
-<% end %>
-
 <%
 
+# Python from here on down.
+
+# Import some modules.
 import numpy as np
+
+NUMS = range(10)
+VALS = [x*x for x in NUMS]
 
 # I can use numpy if I want.
 mysum = (np.array(VALS)* 100.0).sum()
 
+# End of Python code. Time to go back to running commands.
 %>
 
+# Run the reporting command.
 report:
 	echo ${mysum} > report.txt
